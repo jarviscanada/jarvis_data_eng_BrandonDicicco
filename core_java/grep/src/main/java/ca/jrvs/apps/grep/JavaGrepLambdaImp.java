b866b72a68e043f8a66c8ebbb2bfa91242871d0a
Brandon Dicicco <brandondicicco97@gmail.com>
2021-08-04 15:28:18 +0000

    Lambda implementation

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -0,0 +1,63 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaGrepLambdaImp extends JavaGrepImp {
+
+  public static void main(String[] args) {
+    if (args.length != 3) {
+      throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
+    }
+
+    JavaGrepLambdaImp javaGrepLambdaImp = new JavaGrepLambdaImp();
+    javaGrepLambdaImp.setRegex(args[0]);
+    javaGrepLambdaImp.setRootPath(args[1]);
+    javaGrepLambdaImp.setOutFile(args[2]);
+
+    try {
+      javaGrepLambdaImp.process();
+    } catch (Exception ex) {
+      javaGrepLambdaImp.logger.error(ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<String> readLines(File inputFile) {
+    List<String> lines = new ArrayList<>();
+
+    try {
+      lines = Files.lines(inputFile.toPath())
+          .collect(Collectors.toList());
+    } catch (IOException ex) {
+      logger.error(ex.getMessage(), ex);
+    }
+
+    return lines;
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+    List<File> files = new ArrayList<>();
+
+    try {
+      // Walk the directory tree starting at rootDir, get only regular files (no directories)
+      // and add to file list
+      files = Files.walk(Paths.get(rootDir))
+          .filter(Files::isRegularFile)
+          .map(Path::toFile)
+          .collect(Collectors.toList());
+    } catch (IOException ex) {
+      logger.error(ex.getMessage(), ex);
+    }
+
+    return files;
+  }
+
+}
