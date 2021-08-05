de51e718f7dc4d041b18ba873d71fb417f7bfaae
Brandon Dicicco <brandondicicco97@gmail.com>
2021-08-05 19:46:09 +0000

    Creating data

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -21,15 +21,21 @@ public class JDBCExecutor {
 
     try {
       Connection connection = dcm.getConnection();
-      Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+      CustomerDAO customerDAO = new CustomerDAO(connection);
+      Customer customer = new Customer();
+      customer.setFirstName("George");
+      customer.setLastName("Washington");
+      customer.setEmail("george.washington@wh.gov");
+      customer.setPhone("(555) 555-6543");
+      customer.setAddress("1234 Main St");
+      customer.setCity("Mount Vernon");
+      customer.setState("VA");
+      customer.setZipCode("22121");
 
-      while (resultSet.next()) {
-        executor.logger.debug(String.valueOf(resultSet.getInt(1)));
-      }
+      customerDAO.create(customer);
 
     } catch (SQLException ex) {
-        executor.logger.error("Error when creating connection and execution SQL statement", ex);
+        executor.logger.error("Error when creating connection and executing SQL statement", ex);
     }
 
   }
