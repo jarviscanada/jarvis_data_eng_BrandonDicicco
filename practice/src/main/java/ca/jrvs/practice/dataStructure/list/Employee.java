6db323b55a0286f698abf396062df0a53c413f22
Brandon Dicicco <brandondicicco97@gmail.com>
2021-08-10 18:52:27 +0000

    Map learning

--- practice/src/main/java/ca/jrvs/practice/dataStructure/list/Employee.java
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/Employee.java
@@ -1,6 +1,30 @@
 package ca.jrvs.practice.dataStructure.list;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class Employee implements Comparable<Employee> {
+
+  public static void main(String[] args) {
+    Map<Employee, List<String>> empStrMap = new HashMap<>();
+
+    Employee amy = new Employee(1, "Amy", 25, 45000);
+    List<String> amyPreviousCompanies = Arrays.asList("TD", "RBC", "CIBC");
+    empStrMap.put(amy, amyPreviousCompanies);
+
+    Employee bob = new Employee(2, "Bob", 25, 40000);
+    List<String> bobPreviousCompanies = Arrays.asList("A&W", "Superstore");
+    empStrMap.put(bob, bobPreviousCompanies);
+
+    System.out.println("Bob hashcode: " + bob.hashCode());
+    System.out.println("Bob value: " + empStrMap.get(bob).toString());
+    System.out.println("Amy hashcode: " + amy.hashCode());
+    System.out.println("Amy value: " + empStrMap.get(amy).toString());
+  }
+
   private int id;
   private String name;
   private int age;
@@ -56,5 +80,23 @@ public class Employee implements Comparable<Employee> {
       return -1;
     else
       return Long.compare(this.salary, o.getSalary());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Employee employee = (Employee) o;
+    return id == employee.id && age == employee.age && salary == employee.salary && Objects
+        .equals(name, employee.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, age, salary);
   }
 }
