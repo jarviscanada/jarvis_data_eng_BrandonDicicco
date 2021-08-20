9c6d2febeb9b607faab8deca55902cb61e211125
Brandon Dicicco <brandondicicco97@gmail.com>
2021-08-20 20:17:07 +0000

    Spring configs

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
@@ -4,8 +4,10 @@ import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.service.Service;
 import ca.jrvs.apps.twitter.util.TweetUtil;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
+@org.springframework.stereotype.Controller
 public class TwitterController implements Controller {
 
   private static final String COORD_SEP = ":";
@@ -13,6 +15,7 @@ public class TwitterController implements Controller {
 
   private Service service;
 
+  @Autowired
   public TwitterController(Service service) {
     this.service = service;
   }
--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDao.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDao.java
@@ -13,7 +13,9 @@ import java.net.URISyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class TwitterDao implements CrdDao<Tweet, String> {
 
   // URI constants
