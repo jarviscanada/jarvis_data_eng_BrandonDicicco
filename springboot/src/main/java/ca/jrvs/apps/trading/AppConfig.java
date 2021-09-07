c2c7afd366d01dd97c97dd2766367ad7dad04a3c
Brandon Dicicco <brandondicicco97@gmail.com>
2021-09-07 15:02:29 +0000

    Initial quote

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -1,6 +1,8 @@
 package ca.jrvs.apps.trading;
 
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -28,5 +30,24 @@ public class AppConfig {
     cm.setMaxTotal(50);
     cm.setDefaultMaxPerRoute(50);
     return cm;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    String jdbcUrl =
+        "jdbc:postgresql://" +
+            System.getenv("PSQL_HOST") + ":" +
+            System.getenv("PSQL_PORT") +
+            "/" +
+            System.getenv("PSQL_DB");
+    String user = System.getenv("PSQL_USER");
+    String password = System.getenv("PSQL_PASSWORD");
+
+    //Never log your credentials/secrets. Use IDE debugger instead
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setUrl(jdbcUrl);
+    basicDataSource.setUsername(user);
+    basicDataSource.setPassword(password);
+    return basicDataSource;
   }
 }
--- springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -1,8 +1,84 @@
 package ca.jrvs.apps.trading.dao;
 
+import ca.jrvs.apps.trading.model.domain.Quote;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class QuoteDao {
+public class QuoteDao implements CrudRepository<Quote, String> {
 
+  private static final String TABLE_NAME = "quote";
+  private static final String ID_COLUMN_NAME = "ticker";
+
+  private static final Logger logger = LoggerFactory.getLogger(QuoteDao.class);
+  private JdbcTemplate jdbcTemplate;
+  private SimpleJdbcInsert simpleJdbcInsert;
+
+  @Autowired
+  public QuoteDao(DataSource dataSource) {
+    jdbcTemplate = new JdbcTemplate(dataSource);
+    simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME);
+  }
+
+  @Override
+  public Quote save(Quote quote) {
+    return null;
+  }
+
+  @Override
+  public <S extends Quote> Iterable<S> saveAll(Iterable<S> entities) {
+    return null;
+  }
+
+  @Override
+  public Optional<Quote> findById(String s) {
+    return Optional.empty();
+  }
+
+  @Override
+  public boolean existsById(String s) {
+    return false;
+  }
+
+  @Override
+  public Iterable<Quote> findAll() {
+    return null;
+  }
+
+  @Override
+  public Iterable<Quote> findAllById(Iterable<String> strings) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public long count() {
+    return 0;
+  }
+
+  @Override
+  public void deleteById(String s) {
+
+  }
+
+  @Override
+  public void delete(Quote entity) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll(Iterable<? extends Quote> entities) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  @Override
+  public void deleteAll() {
+
+  }
 }
